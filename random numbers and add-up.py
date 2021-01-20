7e14e168ab3ba1b7a4b3744ab61331bc09a4021d
chhwu <76684717+chhwu@users.noreply.github.com>
2021-01-20 16:44:21 +0800

    Update random numbers and add-up.py

--- random numbers and add-up.py
+++ random numbers and add-up.py
@@ -3,7 +3,7 @@ import random
 r_total = 0
 for i in range(3):
     r = random.randint(1, 100)
-    print(i + 1, 'times produce:', r)
+    print(r, 'is produced in', i + 1, 'times.')
     if i >= i - 1:
         r_total += r
 print('Produced', i + 1, 'numbers.')
